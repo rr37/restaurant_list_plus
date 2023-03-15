90e4df04352aaf2485b8966088a5415b985e4e83
rr37 <ay870527@gmail.com>
2023-03-15 11:12:53 +0800

    Render website using html data

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+// require packages used in the project
 const express = require('express')
 const app = express()
-
 const port = 3000
 
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// routes setting
 app.get('/',(req, res) =>{
-  // req.render
-  res.send('Testing ...')
+  res.render('index')
 })
 
 app.listen(port, (req, res) => {
-  console.log('This is a express server')
+  console.log(`Express is listening on localhost: ${port}`)
 })
