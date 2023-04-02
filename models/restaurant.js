e8033eadc80e9cc5ce96d9c4c23af696528349bc
rr37 <ay870527@gmail.com>
2023-04-02 11:54:19 +0800

    feat: add restaurant seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -38,4 +38,4 @@ const restaurantSchema = new Schema({
     required: true
   }
 })
-module.exports = mongoose.model('RESTAURANT', restaurantSchema)
+module.exports = mongoose.model('Restaurant', restaurantSchema)
