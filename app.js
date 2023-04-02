3b58f729f809eac58c67fab42b9c4dd62285e366
rr37 <ay870527@gmail.com>
2023-04-02 10:42:30 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 // require packages used in the project
 const express = require('express')
+// require mongoose
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 const port = 3000
+
+
 
 const exphbs = require('express-handlebars')
 const restaurantList = require('./models/seeds/restaurant.json')
