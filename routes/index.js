c26ff9c864b37d921926a07c69d20d703d68a292
rr37 <ay870527@gmail.com>
2023-04-30 22:17:48 +0800

    feat: add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,12 @@ const restaurants = require('./modules/restaurant')
 // 引入 home 模組程式碼
 const home = require('./modules/home')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
 // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組
-router.use('/restaurants', restaurants)
+router.use('/restaurants', authenticator, restaurants)
 router.use('/users', users)
 // 將網址結構符合 / 字串的 request 導向 home 模組
-router.use('/', home)
+router.use('/', authenticator,  home)
 // 匯出路由器
 module.exports = router
