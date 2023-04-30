381ea11e348f3cff09e774a04d9f5e8995b310a8
rr37 <ay870527@gmail.com>
2023-04-30 21:28:00 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ const express = require('express')
 const router = express.Router()
 // 載入 Restaurant model
 const restaurants = require('./modules/restaurant')
-// 準備引入路由模組
 // 引入 home 模組程式碼
 const home = require('./modules/home')
+const users = require('./modules/users')
 // 將網址結構符合 / 字串的 request 導向 home 模組
 router.use('/', home)
 // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 // 匯出路由器
 module.exports = router
