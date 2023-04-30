e668e5009f927e10cb1b9ab5a72327cac7891ee9
rr37 <ay870527@gmail.com>
2023-04-30 22:11:42 +0800

    feat: add logout feature

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,11 @@ const restaurants = require('./modules/restaurant')
 // 引入 home 模組程式碼
 const home = require('./modules/home')
 const users = require('./modules/users')
-// 將網址結構符合 / 字串的 request 導向 home 模組
-router.use('/', home)
+
 // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組
 router.use('/restaurants', restaurants)
 router.use('/users', users)
+// 將網址結構符合 / 字串的 request 導向 home 模組
+router.use('/', home)
 // 匯出路由器
 module.exports = router
