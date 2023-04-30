156237237a86ce90174c0ee8f58a1fd9afc4b44e
rr37 <ay870527@gmail.com>
2023-04-30 23:21:28 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const helpers = require('handlebars-helpers')()
 const bodyParser = require('body-parser')
 // 載入 method-override
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 // 引用路由器
 const routes = require('./routes')
 // 引用mongoose config
@@ -27,10 +28,13 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
